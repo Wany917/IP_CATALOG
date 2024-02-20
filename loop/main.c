7e814c5101e0b6bc29badb995e32e83b4f4a38dd
Quaume <comaloisi@gmail.com>
2024-02-21 00:20:25 +0100

    search to do - add mask - display addresses binary/hex etc

--- loop/main.c
+++ loop/main.c
@@ -7,6 +7,7 @@
 void menu();
 void addIP(FILE *catalogue);
 void deleteIP(FILE *catalogue);
+void displayIP(FILE *catalogue);
 
 const char filename[] = "catalogue.txt";
 
@@ -34,7 +35,7 @@ int main(){
                 break;
 
                 case 'l':
-
+                    displayIP(catalogue);
                 break;
 
                 case 's':
@@ -42,7 +43,7 @@ int main(){
                 break;
 
                 case 'd':
-
+                    deleteIP(catalogue);
                 break;
 
                 case 'q':
@@ -89,7 +90,7 @@ void addIP(FILE *catalogue){
 
     regi = regcomp(&regex, pattern, REG_EXTENDED);
     if (regi) {
-        fprintf(stderr, "\n   Impossible de compiler l'expression régulière\n");
+        fprintf(stderr, "\n   Error while compilating regex !\n");
         exit(1);
     }
     char addr[20];
@@ -98,17 +99,39 @@ void addIP(FILE *catalogue){
     regi = regexec(&regex, addr, 0, NULL, 0);
     if (!regi) {
 
-        catalogue = fopen(filename, "a+");
-        fprintf(catalogue, "%s\n", addr);
-        fclose(catalogue);
-        printf("\n    IP successfully added !\n");
+        catalogue = fopen(filename, "r");
+        if (catalogue == NULL) {
+            printf("Error while trying to open the file %s.\n", filename);
+            exit(1);
+        }
+
+        char line[50];
+
+        int isIP = 0;
+
+        while (fgets(line, sizeof(line), catalogue) != NULL) {
+            if (strstr(line, addr) != NULL) {
+                isIP = 1;
+            }else{
+                isIP = 0;
+            }
+        }
+
+        if(isIP == 0){
+            catalogue = fopen(filename, "a+");
+            fprintf(catalogue, "%s\n", addr);
+            fclose(catalogue);
+            printf("\n    IP successfully added !\n");
+        }else if(isIP == 1){
+            printf("\n    Ip : %s is already registered !\n", addr);
+        }
 
     } else if (regi == REG_NOMATCH) {
         printf("\n    Wrong IP format !\n");
     } else {
         char msgbuf[100];
         regerror(regi, &regex, msgbuf, sizeof(msgbuf));
-        fprintf(stderr, "\n   Erreur de regex : %s\n", msgbuf);
+        fprintf(stderr, "\n   Regex error : %s\n", msgbuf);
         exit(1);
     }
 
@@ -118,5 +141,70 @@ void addIP(FILE *catalogue){
 
 
 void deleteIP(FILE *catalogue){
+
+    FILE *check;
+    char line[50];
+    char addr[50];
+    int isIP = 0;
+
+    printf("    Enter Ip do you want to remove : ");
+    scanf(" %s", addr);
+
+    catalogue = fopen(filename, "r+");
+    if (catalogue == NULL) {
+        printf("    Error while trying to open : %s !\n", filename);
+        exit(1);
+    }
+
+    check = fopen("check.txt", "w");
+    if (check == NULL) {
+        printf("    Error while trying to create check file !\n");
+        fclose(check);
+        exit(1);
+    }
+
+    while (fgets(line, sizeof(line), catalogue) != NULL) {
+        if (strstr(line, addr) == NULL) {
+            fputs(line, check);
+        }
+    }
+
+    fclose(catalogue);
+    fclose(check);
+
+    remove(filename);
+
+    rename("check.txt", filename);
+
+    printf("\n    Ip : \"%s\" successfully removed !\n", addr);
+
+}
+
+void displayIP(FILE *catalogue){
+
+    char line[50];
+
+    catalogue = fopen(filename, "r");
+    if (catalogue == NULL) {
+        printf("    Impossible d'ouvrir le fichier d'ips !\n");
+        exit(1);
+    }
+
+    printf("    Liste des IPS :\n\n");
+    while (fgets(line, sizeof(line), catalogue) != NULL) {
+        printf("    %s", line);
+    }
+
+    fclose(catalogue);
+
+    printf("\n    Enter r to return to menu !\n");
+    
+    char ret = ' ';
+
+    scanf(" %s", &ret);
+
+    while(ret != 'r'){
+        scanf(" %s", &ret);
+    }
 
 }
