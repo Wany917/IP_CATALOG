6839e510ee4263e90ef91421c03e0faff52523ec
Wany917 <yvannkakou03@gmail.com>
2024-02-21 15:15:53 +0100

    set up sqlite + gtk+3

--- define/declare_func.h
+++ define/declare_func.h
@@ -4,7 +4,4 @@
 
 #ifndef DECLARE_FUNC_H
 #define DECLARE_FUNC_H
-void add_ip(FILE *catalogue);
-void delete_ip(FILE *catalogue);
-void display_ip(FILE *catalogue);
 #endif //DECLARE_FUNC_H
--- main.c
+++ main.c
@@ -1,21 +1,6 @@
-#include <gtk/gtk.h>
+#include "conf.inc.h"
 
-/*
-* Il semble y avoir quelques problèmes dans votre programme, principalement liés à votre expression régulière et à la manière dont elle est utilisée. Voici quelques points à corriger ou à considérer :
-
-Expression régulière : Votre expression régulière actuelle est ^\\d{1,3}[.]\\d{1,3}\[.]\\d{1,3}\[.]\\d{1,3}$, mais elle contient des erreurs. Elle devrait être ^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$. Les caractères [] autour des points sont incorrects et devraient être supprimés. De plus, les points (.) doivent être échappés par des backslashes (\).
-
-Utilisation de regcomp() : Le troisième argument de regcomp() est un ensemble de drapeaux qui modifient le comportement de l'expression régulière. Pour une correspondance basique, vous pouvez utiliser REG_EXTENDED pour permettre l'utilisation d'expressions régulières étendues.
-
-Validation de l'adresse IP : L'expression régulière que vous utilisez ne valide que la forme de l'adresse IP, pas sa validité. Par exemple, elle accepterait "999.999.999.999" comme une adresse IP valide, ce qui n'est pas le cas. Vous pourriez avoir besoin d'une logique supplémentaire pour valider les parties individuelles de l'adresse IP.
-
-Gestion des erreurs : Votre gestion des erreurs avec regerror() est bonne, mais assurez-vous de comprendre les messages d'erreur générés pour déboguer efficacement.
-
-Limitation de la taille de l'entrée : Vous utilisez scanf("%20s", addr) pour limiter la taille de l'entrée à 20 caractères. Cela est bien, mais soyez conscient que cela n'empêche pas les entrées incorrectes. La validation via l'expression régulière est toujours nécessaire.
-
-En résumé, corrigez votre expression régulière et assurez-vous que les drapeaux et les paramètres que vous utilisez avec les fonctions de regex sont correctement définis. Voici comment vous pourriez modifier l'expression régulière et l'utilisation de regcomp() :
- */
 int main (int argc,char **argv)
-{
-
+{   
+    return 0;
 }
